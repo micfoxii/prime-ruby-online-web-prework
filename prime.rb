79f3f458ef43e1eeab12f9ae7b8242c2456068d3
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 16:29:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
+def prime?(n)
+  if n < 2
+    return false
+  end
+  (2...n).each do|x|
+    if (n%x).zero?
+    return false
+  end
+  end
+return true 
+end
 # Add  code here!
+# prime number is whole number, not a fraction, not divisible by any number
