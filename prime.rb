0bbd3127ef3eb92eebbb4d9a6823020a6d6e0512
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 16:15:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,9 @@ def prime?(n)
   else (2..n) do |x|
     (n%x).zero?
     return false
-
+  end
+  end
+true 
+end
 # Add  code here!
 # prime number is whole number, not a fraction, not divisible by any number
