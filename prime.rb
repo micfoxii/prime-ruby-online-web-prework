6ef5b01911fb26967e30d86d0262ea6106d0304f
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 16:05:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
+def prime?(n)
+  number_range = Array.new 
+  number_range.push(n)
+  number_range.each do |x|
+    if n != Integer
+      return false
+    else (n%x).zero?
+      return false
+    end
+    end
+    true
+  end
+
+
 # Add  code here!
+# prime number is whole number, not a fraction, not divisible by any number
