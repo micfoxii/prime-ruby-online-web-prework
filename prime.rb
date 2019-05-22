ff7b25219a7ec5c491ece8ccd3fac22b4ec7c71b
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 16:09:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(n)
   if n <= 1 
     return false 
-  else (2..n) do |
+  else (2..n) do |Integer|
+    n%
 
 # Add  code here!
 # prime number is whole number, not a fraction, not divisible by any number
